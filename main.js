ca51cfde7328b1f77de4cc1682bc8b1dbd18e8c6
charles <charlesjones1088@gmail.com>
2015-05-26 18:27:40 -0600

    Attempting fix for Setup script

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /**
  * Created by charlesjones on 5/26/15.
  */
+require('initial');
 var courier = require("courier");
 var builder = require("builder");
 var spawn = require("spawn");
@@ -14,7 +15,7 @@ var totBuilders = 0;
 var totTransfer = 0;
 var totWorkers =0;
 var totWarriors=0;
-var setup = new Setup();
+var setup = new Setup;
 
 
 //ADD SEPARATE MODULE JUST FOR SPAWN LOGIC AND ROLE ASSIGNMENT
