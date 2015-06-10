1aac2fe551e2d29ae43368af9b4e4c2327769f9e
Charles Jones <charlesjones1088@gmail.com>
2015-06-09 19:18:02 -0600

    Adding multiroom logic for builder

--- builder.js
+++ builder.js
@@ -9,27 +9,35 @@
  * var mod = require('builder'); // -> 'a thing'
  */
 module.exports = function(creep){
-    if(creep.energy == 0 && Game.spawns.Spawn1.energy > 375) {
+    if(creep.energy == 0 && Memory.workers > 1 && Memory.transfers > 1 ) {
         creep.moveTo(Game.spawns.Spawn1);
         Game.spawns.Spawn1.transferEnergy(creep);
     }
+    else if(Game.flags.bMove != undefined)
+    {
+        creep.moveTo(Game.flags.bMove);
+    }
     else{
         var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-        console.log(targets);
-        console.log(targets.length);
         if(targets.length) {
             creep.moveTo(targets[0]);
             creep.build(targets[0]);
         }
-        else {
-            targets = creep.room.find(FIND_MY_STRUCTURES, {filter:function(object)
-                {
-                    if(object.hits < object.hitsMax)
+        else{
+            if(creep.memory.target == "none" || creep.memory.target == undefined) {
+                targets = creep.room.find(FIND_MY_STRUCTURES, {filter: function(object) {
+                    if(object.hits < object.hitsMax *.5)
                         return object;
-                }
-            });
-            creep.moveTo(targets[0]);
-            creep.repair(targets[0]);
+                }});
+                creep.memory.target = targets[0];
+            }
+            else {
+                var target = creep.memory.target;
+                creep.moveTo(target);
+                creep.repair(target);
+                if(target.hits == target.hitsMax)
+                    creep.memory.target = "none";
+            }
         }
     }
 };
