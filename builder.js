b6f55768b08f1f56a45201a5c77f574c6eabbcf1
Charles Jones <charles.jones@domo.com>
2015-06-11 10:15:01 -0600

    Fixing builder logic

--- builder.js
+++ builder.js
@@ -6,55 +6,24 @@
  * var mod = require('builder'); // -> 'a thing'
  */
 module.exports = function(creep) {
-    var spawn = creep.memory.home;
-    spawn = Game.getObjectById(spawn.id);
-
-    var target;
-    if(creep.memory.target == "none") {
-        target = getTarget(creep,creep.memory.task);
-    }
-    target = Game.getObjectById(creep.memory.target.id);
-
+    var targets;
     if(creep.energy == 0 && Memory.workers > 1 && Memory.transfers > 1 ) {
-        creep.moveTo(spawn);
-        spawn.transferEnergy(creep);
+        creep.moveTo(Game.spawns.Spawn1);
+        Game.spawns.Spawn1.transferEnergy(creep);
     }
-    else if(creep.memory.target != 'none' && creep.memory.task != 'construction') {
-        creep.moveTo(target);
-        creep.build(target);
-        if(target.progress >= target.progressTotal)creep.memory.target = "none";
-    }
-    else if(creep.memory.target != 'none' && creep.memory.task == "construction") {
-
-    }
-    else if(Game.flags.bMove != undefined) {
-        var targets = Game.flags.bMove.room.find(FIND_CONSTRUCTION_SITES);
+    else if(Game.flags.bMove) {
+        creep.moveTo(Game.flags.bMove);
+        targets = creep.room.find(FIND_CONSTRUCTION_SITES);
         if(targets.length) {
             creep.moveTo(targets[0]);
             creep.build(targets[0]);
         }
     }
-}
-
-function getTarget(creep,task) {
-    var target;
-    if(task == "construction") {
-        target = creep.room.find(FIND_CONSTRUCTION_SITES);
+    else {
+        targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+        if(targets.length) {
+            creep.moveTo(targets[0]);
+            creep.build(targets[0]);
+        }
     }
-    if(task == "roads" || target == 'undefined') {
-        targets = creep.room.find(FIND_STRUCTURES, {filter: function(object) {
-            if(object.hits < object.hitsMax*.5 && object.structureType == STRUCTURE_ROAD)return object;
-        }})
-    }
-    if(task == "roads"|| target.length==0) {
-        targets = creep.room.find(FIND_STRUCTURES, {filter: function(object) {
-            if(object.hits < object.hitsMax*.5 && object.structureType == STRUCTURE_ROAD)return object;
-        }})
-    }
-    if(task == "ramparts"|| target.length==0) {
-        targets = creep.room.find(FIND_STRUCTURES, {filter: function(object) {
-            if(object.hits < 1000000 && (object.structureType == STRUCTURE_RAMPART || object.structureType == STRUCTURE_WALL))return object;
-        }})
-    }
-    return target[0];
-}
+};
